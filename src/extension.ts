ed4cae18a9105542352e8dce39c006098b1ce037
Neil Macneale V <neil.macneale.v@gmail.com>
2023-01-13 10:56:35 -0800

    Add run query keybind

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
+import { Client, QueryCheckError, QueryRuntimeError, endpoints, fql } from "fauna";
 
 import {
   LanguageClient,
@@ -15,13 +16,44 @@ let client: LanguageClient;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const outputChannel = vscode.window.createOutputChannel('FQL');
+
+  const fqlClient = new Client({
+    endpoint: endpoints.local,
+    max_conns: 5, // maximum number of connections to keep alive and awaiting requests to Fauna
+    secret: "secret",
+    timeout_ms: 60_000
+  });
+
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('fql.helloWorld', () => {
+  let disposable = vscode.commands.registerCommand('fql.runQuery', async () => {
     // The code you place here will be executed every time your command is executed
     // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World from fql!');
+    const { activeTextEditor } = vscode.window;
+
+    if (!activeTextEditor || activeTextEditor.document.languageId !== "fql") {
+      vscode.window.showWarningMessage("You have to select a FQL document to run a FQL query.");
+      return;
+    }
+
+    const text = activeTextEditor.document.getText();
+
+    outputChannel.clear();
+    outputChannel.show(true); // don't move the cursor off the text editor
+    try {
+      var response = await fqlClient.query({ query: text, format: "decorated", typecheck: true } as any);
+      outputChannel.append(response.data);
+    } catch (e) {
+      if (e instanceof QueryCheckError) {
+        outputChannel.append(e.summary ?? "");
+      } else if (e instanceof QueryRuntimeError) {
+        outputChannel.append(e.summary ?? "");
+      } else {
+        outputChannel.append((e as any).toString());
+      }
+    }
   });
 
   context.subscriptions.push(disposable);
